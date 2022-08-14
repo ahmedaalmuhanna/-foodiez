258103813ea7e1313c8bc3253e18fe6fc39aae43
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-08-14 17:50:41 +0300

    first commit

---
+++ Foodiz_venv/Foodiz_proj/Foodiz_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+"""
+IN_CLASS_VARIABLE = models.OneToOneField(
+    CLASS_NAME_RELASHIN, on_delete=models.CASCADE, related_name="NAME_OF_THE_CLASS"
+    
+    IN_CLASS_VARIABLE = models.ManyToManyField(CLASS_NAME_RELASHIN, related_name="NAME_OF_THE_CLASS")
+
+
+"""
+# Create your models here.
